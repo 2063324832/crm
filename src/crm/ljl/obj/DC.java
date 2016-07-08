92e9000a36d5db6c7bb6db06728a4d05e7c67ded
2063324832 <2063324832@qq.com>
2016-07-08 20:45:22 +0800

    create dc

---
+++ src/crm/ljl/obj/DC.java
@@ -0,0 +1,7 @@
+package crm.ljl.obj;
+
+public class DC {
+
+	
+}
+
--- src/crm/ljl/obj/First.java
+++ src/crm/ljl/obj/First.java
@@ -5,5 +5,6 @@ public class First {
 	public static void main(String[] args) {
 		
 		System.out.println("1111111");
+		System.out.println("222222");
 	}
 }
